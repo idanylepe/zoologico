18975b6b06a10680a455c2a6346eb8895a3df7c2
idanylepe <61893153+idanylepe@users.noreply.github.com>
2020-03-31 22:39:01 -0600

    Update zoo.py

--- zoo.py
+++ zoo.py
@@ -39,4 +39,43 @@ while not salir:
  
     opcion = Menu()
  
-    
+    if opcion == 1:
+     a=int (input ("Cuantos animales quieres añadir? \n"))
+     for x in range (a):
+            oof=input("Dame el nombre del animal \n")
+            lista_animal.append(oof)
+            p_tipo=input("Que tipo de animal es?\n")
+            lista_tipo.append(p_tipo)
+    elif opcion == 2:
+        lista_mkgasto = lista_animal+lista_tipo
+        for element in lista_mkgasto:
+         print (element)
+    elif opcion == 3:
+        cambio= int(input ("Deseas cambiar algo en una de las dos listas?\n1.Si\n2.No\n"))
+        if cambio== 1:
+         decision= input("A los nombres o a los tipos de los animales?\n ")
+         if decision==("nombres"):
+          for element in lista_animal:
+           print(lista_animal.index(element), element)          
+          b=int (input("Dame el id del animal que quieres cambiar\n"))
+          lista_animal[b] = input("Nombre nuevo del animal?\n")
+          print ("Lista nueva")
+          for element in lista_animal:
+           print (element)
+         if decision==("tipos"):
+          for element in lista_tipo:
+           print(lista_tipo.index(element), element)          
+          b=int (input("Dame el id del tipo de animal que quieres cambiar\n"))
+          lista_tipo[b] = input("Nombre nuevo del animal?\n")
+          print ("Lista nueva")
+          for element in lista_tipo:
+           print (element)
+         
+        if cambio== 2:
+         print("")
+    elif opcion == 4:
+        salir = True
+    else:
+        print ("Esa no es una opcion, intenta de nuevo")
+ 
+print ("Fin")
